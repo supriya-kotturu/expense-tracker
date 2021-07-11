925e1ec1da4fc35fadf0e0d2a88a16b215eeb40a
supriya-kotturu <supriyakotturu@pm.me>
2021-07-11 17:08:46 +0530

    add : readME.md and theme colors

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# Expense Tracker
+
+A minimalistic application to keep track of your daily expenses.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: "#0B132B",
+        secondary: "#6FFFE9",
+        "dark-001": "#0B132B",
+        "dark-002": "#1C2541",
+        "medium-001": "#3A506B",
+        "color-001": "#5BC0BE",
+        "color-002": "#6FFFE9",
+        "white-001": "#d8def0",
+        "white-002": "#e9ebf0",
+      },
       fontFamily: {
         serif: ["Quicksand", ...defaultTheme.fontFamily.serif],
       },
