6c9bd284cc02e13e0c4858645f7c48f02a654bc1
supriya-kotturu <supriyakotturu@pm.me>
2021-08-08 00:15:42 +0530

    add : initial files for Chart feature

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import ChartBar from "./ChartBar";
+const Chart = (props) => {
+  return <div></div>;
+};
+
+Chart.propTypes = {};
+
+export default Chart;
---
+++ src/components/chart/ChartBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ChartBar = (props) => {
+  return <div></div>;
+};
+
+ChartBar.propTypes = {};
+
+export default ChartBar;
